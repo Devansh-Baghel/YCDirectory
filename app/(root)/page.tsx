0295e1ea223a93bbab8a74e35e2967f221af686f
Nikhil2005Rai <n2005rai@gmail.com>
2025-01-01 18:01:22 +0530

    add ssr

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import StartupCard from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
+import { sanityFetch } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: {
@@ -9,7 +10,9 @@ export default async function Home({ searchParams }: {
 
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  // const posts = await client.fetch(STARTUPS_QUERY)
+
+  const { data: posts } = await sanityFetch({query: STARTUPS_QUERY})
 
   return (
     <>
@@ -23,21 +26,21 @@ export default async function Home({ searchParams }: {
         <SearchForm query={query} />
       </section>
 
-      <div className="section_container">
+      <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for "${query}"` : "All Ideas"}
+          {query ? `Search results for "${query}"` : "All Startups"}
         </p>
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard) => (
+            posts.map((post: any) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
             <p className="no-result">No Startups Found</p>
           )}
         </ul>
-      </div>
+      </section>
     </>
   );
 }
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -9,7 +9,7 @@ const SearchForm = ({ query }: { query?: string }) => {
         name="query"
         defaultValue={query}
         className="search-input"
-        placeholder="Search Ideas"
+        placeholder="Search Startups"
       />
 
       <div className="flex gap-2">
