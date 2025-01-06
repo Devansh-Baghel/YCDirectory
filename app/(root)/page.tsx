03279f059ad84574f89720ca96b2f139875d789b
Devansh Baghel <devanshbaghel85@gmail.com>
2025-01-06 18:08:03 +0530

    disable cache on /

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,27 +4,33 @@ import StartupCard from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
-export default async function Home({ searchParams }: {
-  searchParams: Promise<{ query?: string }>
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
 }) {
-
   const query = (await searchParams).query;
-  const params = { search: query || null };
+  const params = { search: query || null, cache: "no-cache" };
   // const posts = await client.fetch(STARTUPS_QUERY)
 
   const session = await auth();
   console.log(session?.id);
 
-
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
+  const { data: posts } = await sanityFetch({
+    query: STARTUPS_QUERY,
+    ...params,
+  });
 
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch Your Idea, <br /> Connect with Enterpreneurs</h1>
+        <h1 className="heading">
+          Pitch Your Idea, <br /> Connect with Enterpreneurs
+        </h1>
 
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
 
         <SearchForm query={query} />
